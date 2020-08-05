e31f298da5f730b48b58319cdf8bb4a8dd3b0e36
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-05 10:23:20 +0200

    Done exercise 2.4
    Removed braces from arrow functions

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   const { course } = props
   return(
     <>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
     </>
   )
 }
@@ -31,14 +31,12 @@ const Part = (props) => {
 
 const Total = (props) => {
   const { parts } = props
-  const totalEx = parts.reduce((s, p) => {
-    return s + p.exercises
-  },0)
+  const totalEx = parts.reduce((s, p) => s + p.exercises,0)
   return(
     <>
-      <p>
-        Number of exercises {totalEx}
-      </p>
+      <h4>
+        total of {totalEx} exercises
+      </h4>
     </>
   )
 }
@@ -55,34 +53,57 @@ const Course = (props) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Using JavaScript',
-        exercises: 20,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map((course)=><Course key={course.id} course={course}/>)}
+    </> 
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
