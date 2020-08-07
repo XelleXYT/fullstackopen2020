ad0f51a45bbd0e76fe6a8abec1482041e6d889bc
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-07 08:39:20 +0200

    Done exercise 3.2

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,4 +1,3 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 
@@ -27,6 +26,10 @@ let persons = [
 
   app.get('/api/persons', (request, response) => {
     response.json(persons)
+  })
+
+  app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
   })
 
 const PORT = 3001
