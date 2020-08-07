79499b0e0b4a930f5c1b3a2b93544175f70c9ce4
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-07 10:18:35 +0200

    Done exercise 3.8

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -28,7 +28,8 @@ let persons = [
     }
   ]
 
-  app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+  morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
   app.get('/api/persons', (request, response) => {
     response.json(persons)
