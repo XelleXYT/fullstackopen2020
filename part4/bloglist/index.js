c6389df42e23a06168e1b7dc151a6e4e636c8130
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-11 10:28:50 +0200

    Done exercise 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,40 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+const logger = require('./utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+const server = http.createServer(app)
 
-morgan.token('body', function (req) { return JSON.stringify(req.body)})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,26 +1,13 @@
 const mongoose = require('mongoose')
 var uniqueValidator = require('mongoose-unique-validator')
 
-const config = require('../utils/config')
-
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
 
-const url = config.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const blogSchema = new mongoose.Schema({
   autor: {
-    type: String
+    type: String,
+    required: true
   },
   title: {
     type: String,
