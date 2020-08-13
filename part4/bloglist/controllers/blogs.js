4998a114dcb0c75e6eecc64636d64a0306b6cc3b
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-13 11:57:52 +0200

    Added try/catch for correct middleware working

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -11,62 +11,77 @@ const getTokenFrom = request => {
     return null
 }
 
-blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog
-        .find({})
-        .populate('user', {username:1,name:1})
+blogsRouter.get('/', async (request, response, next) => {
+    try{
+        const blogs = await Blog
+            .find({})
+            .populate('user', {username:1,name:1})
 
-    response.json(blogs.map(b => b.toJSON()))
+        response.json(blogs.map(b => b.toJSON()))
+    } catch (e) {
+        next(e)
+    }
 })
 
-blogsRouter.post('/', async (request, response) => {
-    
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
-        return response.status(401).json({ error: 'token missing or invalid' })
-    }
-    const user = await User.findById(decodedToken.id)
-
-    if(!request.body.title || !request.body.url) {
-        return response.status(400).json({
-            error: 'Title and/or URL are missing'
-        })  
-    }
-
-    const likes = 0
-
-    const blog = new Blog(
-        {
-            author: request.body.author,
-            title: request.body.title,
-            url: request.body.url,
-            likes,
-            user
+blogsRouter.post('/', async (request, response, next) => {
+    try {
+        const token = getTokenFrom(request)
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
         }
-    )
+        const user = await User.findById(decodedToken.id)
 
-    const result = await blog.save()
-    response.status(201).json(result)
-})
+        if(!request.body.title || !request.body.url) {
+            return response.status(400).json({
+                error: 'Title and/or URL are missing'
+            })  
+        }
+    
+        const likes = 0
 
-blogsRouter.delete('/:id', async (request, response) => {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-})
+        const blog = new Blog(
+            {
+                author: request.body.author,
+                title: request.body.title,
+                url: request.body.url,
+                likes,
+                user
+            }
+        )
 
-blogsRouter.put('/:id', async (request, response) => {
-    const body = request.body
-
-    const blog = {
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes || 0
+        const result = await blog.save()
+        response.status(201).json(result)
+    } catch (e) {
+        next(e)
     }
+})
 
-    const result = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true, runValidators: true, context: 'query'})
-    response.json(result)
+blogsRouter.delete('/:id', async (request, response, next) => {
+    try{
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch (e) {
+        next(e)
+    }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+    try{
+        const body = request.body
+
+        const blog = {
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes || 0
+        }
+
+        const result = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true, runValidators: true, context: 'query'})
+        response.json(result)
+    } catch (e) {
+        next(e)
+    }
 })
 
 module.exports = blogsRouter
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,34 +2,42 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
-usersRouter.get('/', async (request, response) => {
-    const users = await User
-        .find({})
-        .populate('blogs', {author:1,title:1,url:1})
-    
-    response.json(users.map(u => u.toJSON()))
+usersRouter.get('/', async (request, response, next) => {
+    try{
+        const users = await User
+            .find({})
+            .populate('blogs', {author:1,title:1,url:1})
+        
+        response.json(users.map(u => u.toJSON()))
+    } catch (e) {
+        next(e)
+    }
 })
 
-usersRouter.post('/', async (request, response) => {
-    if (!request.body.password || request.body.password.length <= 3) {
-        return response.status(400).json({
-            'error':'Invalid password'
-        })
-    }
-
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(request.body.password, saltRounds)
-
-    const user = new User(
-        {
-            username: request.body.username,
-            name: request.body.name,
-            passwordHash
+usersRouter.post('/', async (request, response, next) => {
+    try{
+        if (!request.body.password || request.body.password.length <= 3) {
+            return response.status(400).json({
+                'error':'Invalid password'
+            })
         }
-    )
 
-    const result = await user.save()
-    response.status(201).json(result)
+        const saltRounds = 10
+        const passwordHash = await bcrypt.hash(request.body.password, saltRounds)
+
+        const user = new User(
+            {
+                username: request.body.username,
+                name: request.body.name,
+                passwordHash
+            }
+        )
+
+        const result = await user.save()
+        response.status(201).json(result)
+    } catch (e) {
+        next(e)
+    }
 })
 
 module.exports = usersRouter
