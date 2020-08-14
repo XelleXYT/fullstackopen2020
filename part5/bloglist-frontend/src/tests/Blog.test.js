7da8af4d523fa08678f9530781b98e9b492ab2b8
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-14 13:55:38 +0200

    Done exercise 5.14

--- part5/bloglist-frontend/src/tests/Blog.test.js
+++ part5/bloglist-frontend/src/tests/Blog.test.js
@@ -1,24 +1,82 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 
-test('renders title and author', () => {
-  const blog = {
-    title: 'TestTitle',
-    author: 'TestAuthor',
-    url: 'TestUrl',
-    user: {
-        username: 'TestUserUsername',
-        name: 'TestUserName'
-    }
+const blog = {
+  title: 'TestTitle',
+  author: 'TestAuthor',
+  url: 'TestUrl',
+  likes: 10,
+  user: {
+      username: 'TestUserUsername',
+      name: 'TestUserName'
   }
+}
 
-  const component = render(
-    <Blog blog={blog} likeBlog={()=>{}} currentUser={{username:'TestUserUsername', user:'TestUserName'}} removeBlog={()=>{}}/>
-  )
+/* 
+  IMPORTANT:
+    I've used the Togglable to do the blogs show/hide info.
+    It renders all content everytime, but shows only what it need to show.
+*/
+describe('<Blog />', () => {
+  test('shows only title and author', () => {
+    const component = render(
+      <Blog blog={blog} likeBlog={()=>{}} currentUser={{username:'TestUserUsername', user:'TestUserName'}} removeBlog={()=>{}}/>
+    )
 
-  const div = component.container.querySelector('.blog')
-  expect(div).toHaveTextContent('TestTitle')
-  expect(div).toHaveTextContent('TestAuthor')
+    const div = component.container.querySelector('.blog')
+    const togglable = component.container.querySelector('.togglableContent')
+
+    expect(div).toHaveTextContent('TestTitle')
+    expect(div).toHaveTextContent('TestAuthor')
+    expect(div).toHaveTextContent('TestUrl')
+    expect(div).toHaveTextContent('10')
+    expect(togglable).toHaveStyle('display: none')
+
+  })
+
+  test('shows url and likes too', () => {
+    const component = render(
+      <Blog blog={blog} likeBlog={()=>{}} currentUser={{username:'TestUserUsername', user:'TestUserName'}} removeBlog={()=>{}}/>
+    )
+    
+    const button = component.getByText('show')
+    fireEvent.click(button)
+
+    const div = component.container.querySelector('.blog')
+    const togglable = component.container.querySelector('.togglableContent')
+
+    expect(div).toHaveTextContent('TestTitle')
+    expect(div).toHaveTextContent('TestAuthor')
+    expect(div).toHaveTextContent('TestUrl')
+    expect(div).toHaveTextContent('10')
+    expect(togglable).not.toHaveStyle('display: none')
+
+  })
+
+  test('hides url and likes', () => {
+    const component = render(
+      <Blog blog={blog} likeBlog={()=>{}} currentUser={{username:'TestUserUsername', user:'TestUserName'}} removeBlog={()=>{}}/>
+    )
+    
+    const buttonShow = component.getByText('show')
+    fireEvent.click(buttonShow)
+
+    const togglableShow = component.container.querySelector('.togglableContent')
+    expect(togglableShow).not.toHaveStyle('display: none')
+
+    const buttonHide = component.getByText('hide')
+    fireEvent.click(buttonHide)
+
+    const div = component.container.querySelector('.blog')
+    const togglableHide = component.container.querySelector('.togglableContent')
+
+    expect(div).toHaveTextContent('TestTitle')
+    expect(div).toHaveTextContent('TestAuthor')
+    expect(div).toHaveTextContent('TestUrl')
+    expect(div).toHaveTextContent('10')
+    expect(togglableHide).toHaveStyle('display: none')
+
+  })
 })
