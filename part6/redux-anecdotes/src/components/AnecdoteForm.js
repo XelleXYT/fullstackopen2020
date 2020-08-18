cf587ad1b89aa3f473f1c01a783cc3cb32e3f849
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-18 11:30:41 +0200

    Done exercise 6.8

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -14,10 +14,13 @@ const AnecdoteForm = () => {
   }
 
   return(
-    <form onSubmit={add}>
-      <div><input name="anecdote" /></div>
-      <button type="submit">create</button>
-    </form>
+    <>
+      <h2>create new</h2>
+      <form onSubmit={add}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </>
   )
 }
 
