5578ec9cd0376d3b65561191ae501e50abc174d4
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-25 10:53:36 +0200

    Done exercise 8.5

--- part8/library/index.js
+++ part8/library/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -112,11 +112,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      return(
-        args.author
-        ? books.filter(b => b.author === args.author)
-        : books
-      )
+      let auxBooks = books
+      if(args.author) {
+        auxBooks = auxBooks.filter(b => b.author === args.author)
+      }
+      if(args.genre) {
+        auxBooks = auxBooks.filter(b => b.genres.includes(args.genre))
+      }
+      return auxBooks
     },
     allAuthors: () => authors
   },
