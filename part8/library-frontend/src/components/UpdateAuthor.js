9fb7b101378d457e9468a0169bf679d6509fae37
Alejandro Luna <33550473+XelleXYT@users.noreply.github.com>
2020-08-25 14:25:06 +0200

    Done exercise 8.12

--- part8/library-frontend/src/components/UpdateAuthor.js
+++ part8/library-frontend/src/components/UpdateAuthor.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
 import { UPDATE_AUTHOR, ALL_AUTHORS } from '../queries'
+import Select from 'react-select'
 
 const UpdateAuthor = (props) => {
   const [name, setName] = useState('')
   const [born, setBorn] = useState('')
+
+  const options = props.authors.map((a)=>{
+    return(
+      {
+        value: a.name,
+        label: a.name
+      }
+    )
+  })
 
   const [ updateAuthor ] = useMutation(UPDATE_AUTHOR, {
     refetchQueries: [ { query: ALL_AUTHORS } ]
@@ -13,7 +23,7 @@ const UpdateAuthor = (props) => {
   const submit = async (event) => {
     event.preventDefault()
 
-    updateAuthor({ variables: { name, born: Number(born) }})
+    updateAuthor({ variables: { name: name.value, born: Number(born) }})
 
     setName('')
     setBorn('')
@@ -24,8 +34,7 @@ const UpdateAuthor = (props) => {
       <h3>Set birthyear</h3>
       <form onSubmit={submit}>
         <div>
-          name
-          <input value={name} onChange={({ target }) => setName(target.value)} />
+          <Select value={name} options={options} onChange={(selectedOption) => setName(selectedOption)} />
         </div>
         <div>
           born
